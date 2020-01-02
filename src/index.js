eefd36b5015f8cbf4fa5f24e9c572d5420a39f41
Emily Baumgartner <emily.r.baum@gmail.com>
2020-01-02 00:26:41 -0500

    added pagination from the tutorial and completed the app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { setContext } from 'apollo-link-context';
 import { AUTH_TOKEN } from './constants';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 // 1
 import { ApolloProvider } from 'react-apollo';
@@ -14,7 +17,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 
-// 2
+// // 2
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
@@ -29,9 +32,34 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
-// 3
+// // 3
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache()
+// })
+
+// -----
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 })
 
